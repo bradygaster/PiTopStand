62c16261ab27a81fe78ba8603730c7a56f86230c
bradygaster <bradyg@microsoft.com>
2022-05-16 18:14:16 -0700

    Add project files.

---
+++ PiTopStand/Program.cs
@@ -0,0 +1,17 @@
+﻿using PiTop;
+using PiTopMakerArchitecture.Foundation;
+
+var board = PiTop4Board.Instance;
+var plate = new FoundationPlate(board);
+
+var greenLed = plate.GetOrCreateLed(DigitalPort.D0);
+var blueLed = plate.GetOrCreateLed(DigitalPort.D1);
+var redLed = plate.GetOrCreateLed(DigitalPort.D2);
+
+greenLed.On();
+blueLed.On();
+redLed.On();
+
+Console.WriteLine("Hit enter to exit");
+Console.ReadLine();
+Console.WriteLine("Exiting");
