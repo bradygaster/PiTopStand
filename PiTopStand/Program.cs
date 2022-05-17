93be82b3831286d7f63c03cb00ece0b8dbc60b59
bradygaster <bradyg@microsoft.com>
2022-05-16 23:11:27 -0700

    mvp

--- PiTopStand/Program.cs
+++ PiTopStand/Program.cs
@@ -5,13 +5,68 @@ var board = PiTop4Board.Instance;
 var plate = new FoundationPlate(board);
 
 var greenLed = plate.GetOrCreateLed(DigitalPort.D0);
-var blueLed = plate.GetOrCreateLed(DigitalPort.D1);
+var yellowLed = plate.GetOrCreateLed(DigitalPort.D1);
 var redLed = plate.GetOrCreateLed(DigitalPort.D2);
 
-greenLed.Toggle();
-blueLed.Toggle();
-redLed.Toggle();
+greenLed.On();
+yellowLed.On();
+redLed.On();
 
-Console.WriteLine("Hit enter to exit");
-Console.ReadLine();
+Console.WriteLine("Pausing");
+
+await Task.Delay(5000);
+
 Console.WriteLine("Exiting");
+
+for (int i = 0; i < 7; i++)
+{
+    greenLed.Toggle();
+    yellowLed.Toggle();
+    redLed.Toggle();
+
+    await Task.Delay(500);
+}
+
+var leftToRight = async (int duration) =>
+{
+    greenLed.Toggle();
+    await Task.Delay(duration);
+
+    greenLed.Toggle();
+    yellowLed.Toggle();
+    await Task.Delay(duration);
+
+    yellowLed.Toggle();
+    redLed.Toggle();
+    await Task.Delay(duration);
+    redLed.Toggle();
+    await Task.Delay(duration);
+};
+
+var rightToLeft = async (int duration) =>
+{
+    redLed.Toggle();
+    await Task.Delay(duration);
+
+    redLed.Toggle();
+    yellowLed.Toggle();
+    await Task.Delay(duration);
+
+    yellowLed.Toggle();
+    greenLed.Toggle();
+    await Task.Delay(duration);
+    greenLed.Toggle();
+    await Task.Delay(duration);
+};
+
+while(true)
+{
+    await leftToRight(100);
+    await rightToLeft(100);
+}
+
+Console.WriteLine("Continuing");
+
+greenLed.Off();
+yellowLed.Off();
+redLed.Off();
